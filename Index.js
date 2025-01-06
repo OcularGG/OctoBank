bb458532133a7e6c4f2795bf0dec0da6536a295b
SEDocotor <158790696+SEDocotor@users.noreply.github.com>
2025-01-06 03:03:06 -0800

    Update Index.js

--- Index.js
+++ Index.js
@@ -814,4 +814,4 @@ client.on('interactionCreate', async (interaction) => {
     }
 
 });
-client.login('MTMyNTU0OTAyMDM1Mjg3NjY0NQ.G6v1IW.h5wWEbcWaRTipcvZAOKQc8hCWWVzC77KdbGsL4');
+client.login('MTMyNDQzMDIzMTEyOTQyODA4OA.GxVrxA.WjoApui9d9bi0iB5HJJaB8ewVBwWNPQZjpTkxc');
