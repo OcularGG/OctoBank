f37ee573c84a8259d718fd166a89e9da41f23dac
SEDocotor <158790696+SEDocotor@users.noreply.github.com>
2025-01-28 21:25:31 -0800

    Fixed DB.js

--- db.js
+++ db.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const dbConfig = {
-    host: 'db-slc-01.sparkedhost.us',
+    host: 'db-buf-05.sparkedhost.us',
     port: 3306,
     user: process.env.DBUSER,
     password: process.env.DBPASS,
-    database: 's159065_Test-Database'
+    database: 's159065_OctoBank'
 };
 
 const pool = mysql.createPool(dbConfig);
